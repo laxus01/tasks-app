bfa38d71e2b5e88371ad0f15163285cd7f020fe3
JOSE TUÑON <ut_josetv@hotmail.com>
2025-10-28 13:12:27 -0500

    Configuración libreria crypto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api.com/api'
+  apiUrl: 'https://tasks-backend-production-36fd.up.railway.app/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://tasks-backend-production-36fd.up.railway.app/'
 };
 
 /*
